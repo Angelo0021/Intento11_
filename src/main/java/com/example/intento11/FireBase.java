76320d662016857ac2f8ac6450710df7af94af89
Angelo <angelocifuentes3@gmail.com>
2021-11-29 19:00:39 -0600

    gg

--- src/main/java/com/example/intento11/FireBase.java
+++ src/main/java/com/example/intento11/FireBase.java
@@ -9,6 +9,6 @@ public class FireBase extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fire_base);
+        setContentView(R.layout.formulario);
     }
 }
